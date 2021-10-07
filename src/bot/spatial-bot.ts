dc42a54784f4b79f91514ac77903898c7703512d
Philo van Kemenade <philovankemenade@gmail.com>
2021-10-07 17:50:24 +0200

    fetch spatial data and calculate relationships, reformat for sonar demo
    also fetch and write data

--- src/bot/spatial-bot.ts
+++ src/bot/spatial-bot.ts
@@ -44,123 +44,133 @@ WHERE {
 
 // TODO: adjust to latest KG model
 const getAnnotationsQuery = `
-        
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX poli-mp: <https://w3id.org/polifonia/ON/musical-performance/>
-        PREFIX poli-core:  <https://w3id.org/polifonia/ON/core/>
+PREFIX core:  <https://w3id.org/polifonia/ON/core/>
+PREFIX pr:    <https://w3id.org/polifonia/resource/>
+PREFIX fx:    <http://sparql.xyz/facade-x/ns/>
+PREFIX mp:    <https://w3id.org/polifonia/ON/musical-performance/>
+PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xyz:   <http://sparql.xyz/facade-x/data/>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
 
-        SELECT ?id 
-               ?type 
-               ?songID
-               ?timestamp
-
-               ?relatedSongID
-               ?placeName
-               ?description
-
-               ?lat
-               ?long
-
-               WHERE {
-            
-          ?id rdf:type poli-core:SpatialAnnotation ;
-              poli-mp:hasDescription ?description ;
-              poli-mp:hasTimeStamp ?timestamp ;
-              poli-mp:hasType ?type ;
-              poli-mp:aboutRecording ?songID ;
-              poli-mp:hasSpatialSimilarityToRecording ?relatedSongID ;
-              poli-mp:hasLat ?lat;
-              poli-mp:hasLng ?long ;
-              poli-mp:aboutPlaceName ?placeName .
-            }
+SELECT ?recordingID ?recordingTitleLabel ?sessionType ?placeLabel ?placeFullAddress ?placeLat ?placeLong
+WHERE {
+  ?recordingID rdf:type mp:Recording ;
+      core:hasTitle ?title ;
+      mp:isRecordingProducedBy ?recordingProcess
+  .
+  ?title rdfs:label ?recordingTitleLabel.
+  ?recordingProcess mp:hasSession ?session.
+  ?session core:hasPlace ?place ;
+           core:hasType ?sessionType.
+  ?place rdfs:label ?placeLabel ;
+         core:hasAddress ?placeAddress ;
+         core:hasGeometry ?placeGeometry
+  .
+  ?placeAddress core:fullAddress ?placeFullAddress.
+  ?placeGeometry core:lat ?placeLat ;
+                 core:long ?placeLong
+  .
+}
 `
 
-const toSonarAppAnnotation =  (sparqlRow : any) => {
+const toSonarSongAnnotation = (sparqlRow: any) => {
+    return {
+        name: sparqlRow.recordingTitleLabel,
+        artist: sparqlRow.performerLabel,
+        artistId: sparqlRow.performerID,
+        id: sparqlRow.recordingID,
+        youtubeID: sparqlRow.youtubeID,
+    };
+};
+
+const toSonarAppAnnotation = (sparqlRow: any) => {
     return {
         id: sparqlRow.id,
-        type: sparqlRow.type,
-        description: sparqlRow.description,
-        songID: sparqlRow.songID,
+        type: "spatial",
+        songID: sparqlRow.recordingID,
+        timestamp: getRandomInt(0, 60),
         metadata: {
-            long: sparqlRow.long,
-            lat: sparqlRow.lat,
-            placeName: sparqlRow.placeName
+            long: sparqlRow.placeLong,
+            lat: sparqlRow.placeLat,
+            placeName: sparqlRow.placeLabel
         },
-        relationships: [{
-            songID: sparqlRow.relatedSongID,
-            type: sparqlRow.type,
-            score: Math.random()
-        }]
-
+        relationships: sparqlRow.relationships
     }
-}
-
-const toSonarSongAnnotation = (sparqlRow: any) => {
-
-    return {
-        name      : sparqlRow.recordingTitleLabel,
-        artist    : sparqlRow.performerLabel,
-        artistId  : sparqlRow.performerID,
-        id        : sparqlRow.recordingID,
-        youtubeID : sparqlRow.youtubeID,
-    }
-}
+};
 
 // remove duplicates with same id
 const withoutDuplicates = (data: any[]) => {
     return data.filter((v : any,i : any,a : any) => a.findIndex((t : any)=>(t.id === v.id))===i)
 }
 
+// get random integer between min and max
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+// add IDs to annotations
+const hydrateAnnotationIDs = (data: any[]) => {
+    let i = 0;
+    return data.map(a => {
+        i++
+        a.id = i
+        return a
+    });
+};
+
+// add relationships to annotations
+const hydrateAnnotationRels = (data: any[], maxRelationships: number) => {
+    return data.map(a => hydrateAnnotationRel(a, data, maxRelationships));
+};
+
+// add relationships to single annotation
+const hydrateAnnotationRel = (a: any, data: any[], maxRelationships: number) => {
+    let relationshipsSpatial = data
+        .filter(anotherA => anotherA.placeID == a.placeID && anotherA.id !== a.id)
+        .map(anotherA => {
+            return {
+                annotationID: anotherA.id,
+                type: "spatial",
+                score: 1,
+            }
+        })
+    a.relationships = relationshipsSpatial.slice(0, maxRelationships)
+    return a;
+};
+
 
 function main() {
-    
     // launch get songs job
     const getSongs = sparqlETL.run({
         query: getSongsQuery,
         sources: sources
-    })
-    
+    });
     // launch get annotation jobs
     const getAnnotations = sparqlETL.run({
         query: getAnnotationsQuery,
         sources: sources
-    })
-    
-
-    // run query in parallel
-    // Promise.all([getSongs, getAnnotations]).then(([songsResults, annotationResults]) => {
-    
-    //     // remove duplicates and map to App Entities
-    //     const sonarAnnotations = withoutDuplicates(annotationResults.map(toSonarAppAnnotation))
-    //     const songAnnotations = withoutDuplicates(songsResults.map(toSonarSongAnnotation))
-
-    //     // write new json static file
-    //     filePublisher.write({
-    //         songs: songAnnotations,
-    //         annotations: sonarAnnotations
-    //     }, {
-    //         destination: "./data_v3.json",
-    //         msg: "[*] File written to: " + "./data_v3.json"
-    //     })
-        
-    // })
-
-    // run query in parallel
-    Promise.all([getSongs]).then(([songsResults]) => {
-    
+    });
+    // run queries in parallel
+    Promise.all([getSongs, getAnnotations]).then(([songsResults, annotationResults]) => {
+        const MAX_RELATIONSHIPS = 3;
         // remove duplicates and map to App Entities
-        const songAnnotations = withoutDuplicates(songsResults.map(toSonarSongAnnotation))
+        const sonarSongs = withoutDuplicates(songsResults.map(toSonarSongAnnotation))
+        const annotationResultsWithID = hydrateAnnotationIDs(annotationResults);
+        const annotationResultsWithRels = hydrateAnnotationRels(annotationResultsWithID, MAX_RELATIONSHIPS);
+        const sonarAnnotations = annotationResultsWithRels.map(toSonarAppAnnotation);
 
         // write new json static file
-        const targetFileName = "polifonia-kg-places-0.0.1-4demo.json"
+        const targetFileName = "polifonia-kg-places-0.0.1-4demo.json";
         filePublisher.write({
-            songs: songAnnotations,
+            songs: sonarSongs,
+            annotations: sonarAnnotations,
         }, {
             destination: `./data-out/${targetFileName}`,
             msg: "[*] File written to: " + `./data-out/${targetFileName}`
-        })
-        
-    })
+        });
+    });
 }
 
 main()
